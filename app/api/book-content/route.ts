adc364fe7a30469504f2960bb254d69213cf946b
Mayank Negi <mayankn792@gmail.com>
2025-04-27 23:11:45 +0530

    code ref

--- app/api/book-content/route.ts
+++ app/api/book-content/route.ts
@@ -1,10 +1,8 @@
-import { GoogleGenAI } from "@google/genai";
 import { NextApiRequest } from "next";
 import { BOOK_CONTENT_SYSTEM_INSTRUCTION } from "../prompts";
-const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
+import { generateContent } from "@/app/api/utils";
 
 export async function GET(request: NextApiRequest) {
-    console.log("request - inside book content", request);
     if (!request.url) {
         return new Response("Invalid request URL", { status: 400 });
     }
@@ -14,22 +12,10 @@ export async function GET(request: NextApiRequest) {
     const bookSubTopic = url.searchParams.get("bookSubTopic");
 
     if (typeof bookTitle !== "string" || typeof bookTopic !== "string" || typeof bookSubTopic !== "string") {
-        return new Response("Invalid book title", { status: 400 });
+        return new Response("Invalid book title or topic or subtopic", { status: 400 });
     }
 
-    const generatedResponse = await generateContent(bookTitle, bookTopic, bookSubTopic);
-    return new Response(generatedResponse);
-}
-
-async function generateContent(bookTitle: string, bookTopic: string, bookSubTopic: string) {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: "book title - " + bookTitle + " book topic - " + bookTopic + " book sub-topic - " + bookSubTopic,
-      config: {
-        systemInstruction: BOOK_CONTENT_SYSTEM_INSTRUCTION,
-      },
-    });
-
-    const parsedResponse = response?.text?.replace(/^```json/, "").replace(/```$/, "");
-    return parsedResponse;
+    const contents = "book title - " + bookTitle + " book topic - " + bookTopic + " book sub-topic - " + bookSubTopic;
+    const generatedResponse = await generateContent(contents, BOOK_CONTENT_SYSTEM_INSTRUCTION);
+    return new Response(generatedResponse.text);
 }
