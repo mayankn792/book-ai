041425795ab1fae07aeb5dbf3228023ee8238c20
Mayank Negi <mayankn792@gmail.com>
2025-04-27 23:06:55 +0530

    code ref

--- app/api/book-index/route.ts
+++ app/api/book-index/route.ts
@@ -1,7 +1,6 @@
-import { GoogleGenAI } from "@google/genai";
 import { NextApiRequest } from "next";
+import { generateContent } from "@/app/api/utils";
 import { BOOK_INDEX_SYSTEM_INSTRUCTION } from "../prompts";
-const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
 
 export async function GET(request: NextApiRequest) {
     if (!request.url) {
@@ -14,19 +13,7 @@ export async function GET(request: NextApiRequest) {
         return new Response("Invalid book title", { status: 400 });
     }
 
-    const generated = await generateContent(bookTitle);
-    return new Response(generated);
-}
-
-async function generateContent(bookTitle: string) {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: "book title is " + bookTitle,
-      config: {
-        systemInstruction: BOOK_INDEX_SYSTEM_INSTRUCTION,
-      },
-    });
-
-    const parsedResponse = response?.text?.replace(/^```json/, "").replace(/```$/, "");
-    return parsedResponse;
+    const contents = "book title - " + bookTitle
+    const generatedResponse = await generateContent(contents, BOOK_INDEX_SYSTEM_INSTRUCTION);
+    return new Response(generatedResponse?.text?.replace(/^```json/, "").replace(/```$/, ""));
 }
--- app/api/utils.ts
+++ app/api/utils.ts
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
-async function generateContent(content: string, systemInstruction: string) {
+export async function generateContent(content: string, systemInstruction: string) {
     const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
