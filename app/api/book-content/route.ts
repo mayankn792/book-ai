5b5f8459cf535892773016b8c0e42ef1caf7c72a
Mayank Negi <mayankn792@gmail.com>
2025-04-30 23:53:54 +0530

    Add book content search function

--- app/api/book-content/route.ts
+++ app/api/book-content/route.ts
@@ -7,15 +7,16 @@ export async function GET(request: NextApiRequest) {
         return new Response("Invalid request URL", { status: 400 });
     }
     const url = new URL(request.url);
-    const bookTitle = url.searchParams.get("bookTitle");
+    // const bookTitle = url.searchParams.get("bookTitle");
     const bookTopic = url.searchParams.get("bookTopic");
     const bookSubTopic = url.searchParams.get("bookSubTopic");
 
-    if (typeof bookTitle !== "string" || typeof bookTopic !== "string" || typeof bookSubTopic !== "string") {
+    //typeof bookTitle !== "string" ||
+    if ( typeof bookTopic !== "string" || typeof bookSubTopic !== "string") {
         return new Response("Invalid book title or topic or subtopic", { status: 400 });
     }
 
-    const contents = "book title - " + bookTitle + " book topic - " + bookTopic + " book sub-topic - " + bookSubTopic;
+    const contents = "book title - " + + " book topic - " + bookTopic + " book sub-topic - " + bookSubTopic;
     const generatedResponse = await generateContent(contents, BOOK_CONTENT_SYSTEM_INSTRUCTION);
     return new Response(generatedResponse.text);
 }
--- components/BookIndex.tsx
+++ components/BookIndex.tsx
@@ -1,11 +1,40 @@
 "use client";
 
+import axios from "axios";
 import { useBookIndexStore } from "@/stores/bookIndex";
+import { useState } from "react";
 
 export default function BookIndex() {
     const { bookIndex } = useBookIndexStore();
-
+    const [bookContent, setBookContent] = useState("");
     return <div>
-        {JSON.stringify(bookIndex)}
+        {
+           bookIndex.map((item: { topic: string; subtopics: string[] }) => (
+             <div key={item.topic}>
+               <h1>topic - {item.topic}</h1>
+               <ul>
+                 {item.subtopics.map((subtopic: string) => (
+                   <li key={subtopic}>
+                   {subtopic}
+                   <button onClick={async () => {
+                     const contentResponse = await axios.get("/api/book-content", {
+                     params: {
+                    //    bookTitle: bookTitle,
+                       bookTopic: item.topic,
+                       bookSubTopic: subtopic,
+                     },
+                     });
+                     console.log("contentResponse", contentResponse.data);
+                     setBookContent(contentResponse.data);
+                   }}>
+                     Get Content
+                   </button>
+                   </li>
+                 ))}
+               </ul>
+             </div>
+           ))
+         }
+        {JSON.stringify(bookContent)}
     </div>
 }
