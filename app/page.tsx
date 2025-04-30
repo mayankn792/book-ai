dbc93e52a817f4a6d3855619acfaf83fcfe1a218
Mayank Negi <mayankn792@gmail.com>
2025-04-30 22:42:14 +0530

    home page ref, move code to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import axios from "axios";
 import BookContentFinder from "@/components/BookContentFinder";
-import { useBookIndexStore } from "@/stores/bookIndex";
 
 export default function Home() {
-  const [bookTitle, setBookTitle] = useState("");
-  const [response, setResponse] = useState([]);
-  const { setBookIndex } = useBookIndexStore();
   return (
-    <div>
-      <BookContentFinder />
-      <br></br>
-      <input type="text" onChange={(event) => setBookTitle(event.target.value)} placeholder="book title goes here ...." />
-      <br></br>
-      <button onClick={async () => {
-        console.log("book title", bookTitle);
-        const response = await axios.get("/api/book-index", {
-          params: {
-            bookTitle: bookTitle,
-          },
-        });
-        console.log("response", response);
-        setBookIndex(response.data);
-        setResponse(response.data);
-      }}>get index</button>
-      <br></br>
-      <div>
-        {
-          response.map((item: { topic: string; subtopics: string[] }) => (
-            <div key={item.topic}>
-              <h1>topic - {item.topic}</h1>
-              <ul>
-                {item.subtopics.map((subtopic: string) => (
-                  <li key={subtopic}>
-                  {subtopic}
-                  <button onClick={async () => {
-                    const contentResponse = await axios.get("/api/book-content", {
-                    params: {
-                      bookTitle: bookTitle,
-                      bookTopic: item.topic,
-                      bookSubTopic: subtopic,
-                    },
-                    });
-                    console.log("contentResponse", contentResponse.data);
-                  }}>
-                    Get Content
-                  </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))
-        }
-      </div>
-    </div>
+    <BookContentFinder />
   );
 }
--- components/BookContentFinder.tsx
+++ components/BookContentFinder.tsx
@@ -1,17 +1,34 @@
 "use client";
+
+import { useState } from "react";
+import axios from "axios";
+
 import { useBookIndexStore } from "@/stores/bookIndex";
 
+
 export default function BookContentFinder() {
-    const { bookIndex } = useBookIndexStore();
+    const [bookTitle, setBookTitle] = useState("");
+    const [response, setResponse] = useState([]);
+    const { bookIndex, setBookIndex } = useBookIndexStore();
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-3xl">book-ai</h1>
             <h5 className="text-md text-gray-600">Get a book content</h5>
             <br></br>
-            <input type="text" placeholder="book title goes here ...." />
+            <input type="text" onChange={(event) => setBookTitle(event.target.value)} placeholder="book title goes here ...." />
             <br></br>
-            <button>get index</button>
-            { JSON.stringify(bookIndex) }
+            <button onClick={async () => {
+                console.log("book title", bookTitle);
+                const response = await axios.get("/api/book-index", {
+                    params: {
+                        bookTitle: bookTitle,
+                    },
+                });
+                console.log("response", response);
+                setBookIndex(response.data);
+                setResponse(response.data);
+            }}>get index</button>
+            {JSON.stringify(bookIndex)}
         </div>
     );
 }
