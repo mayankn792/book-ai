038ffb2360fc5c3ff5ce7c22c107ff8b907667f2
Mayank Negi <mayankn792@gmail.com>
2025-05-02 00:56:09 +0530

    imporove textarea

--- components/BookContentFinder.tsx
+++ components/BookContentFinder.tsx
@@ -13,11 +13,13 @@ export default function BookContentFinder() {
     return (
         <div className="flex flex-row">
             <div className="basis-2/12"></div>
-            <div className="basic-8/12 flex flex-col justify-center min-h-screen py-2">
+            <div className="basic-8/12 flex flex-col justify-center min-h-screen py-2 antialiased">
                 <h5 className="text-2xl">What do you want us to generate?</h5>
-                <br></br>
-                <textarea className="appearance-none border-none p-0 font-inherit text-inherit outline-none bg-transparent resize-none" onChange={(event) => setBookTitle(event.target.value)} placeholder="book title goes here ...." />
-                <br></br>
+                
+                <div className="bg-white p-5 text-blue-600">
+                <textarea rows={4} cols={50} className="appearance-none border-none p-0 font-inherit text-inherit outline-none bg-transparent resize-none" onChange={(event) => setBookTitle(event.target.value)} placeholder="book title goes here ...." />
+                </div>
+                
                 <button className="btn btn-primary" onClick={async () => {
                     console.log("book title", bookTitle);
                     const response = await axios.get("/api/book-index", {
