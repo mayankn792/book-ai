666a267b325cead70f7234122ae3963da1e23418
Mayank Negi <mayankn792@gmail.com>
2025-05-04 22:25:12 +0530

    ref indexes from book context

--- components/BookIndex.tsx
+++ components/BookIndex.tsx
@@ -2,55 +2,56 @@
 
 import axios from "axios";
 import { useBookIndexStore } from "@/stores/bookIndex";
-import { useBookStore, BookTopic } from "@/stores/book";
+import { useBookStore, BookTopic, BookSubTopic } from "@/stores/book";
 import { useContextStore } from "@/stores/context";
 import { useState } from "react";
 
 interface TitleProp {
-    title: string | null,
+  title: string | null,
 }
 
 export default function BookIndex({ title }: TitleProp) {
-    const { bookIndex } = useBookIndexStore();
-    const { books, addBookTopic, addBookSubTopic } = useBookStore();
-    const { setGlobalBookTopic, setGlobalBookSubTopic, setGlobalContent } = useContextStore();
-    const [bookContent, setBookContent] = useState("");
-    console.log(bookIndex);
-    console.log("-----books------");
-    console.log(books);
-    return <div>
-        {
-          //TODO - remove bookIndex and use bookContext
-           bookIndex.map((item: { topic: string; subtopics: string[] }) => (
-             <div key={item.topic}>
-               <h1>topic - {item.topic}</h1>
-               <ul>
-                 {item.subtopics.map((subtopic: string) => (
-                   <li key={subtopic}>
-                   {subtopic}
-                   <button onClick={async () => {
-                     const contentResponse = await axios.get("/api/book-content", {
-                     params: {
-                    //    bookTitle: bookTitle,
-                       bookTopic: item.topic,
-                       bookSubTopic: subtopic,
-                     },
-                     });
-                     
-                     console.log("contentResponse", contentResponse.data);
-                     setBookContent(contentResponse.data);
+  const { bookIndex } = useBookIndexStore();
+  const { books, addBookTopic, addBookSubTopic } = useBookStore();
+  const { bookContext, setGlobalBookTopic, setGlobalBookSubTopic, setGlobalContent } = useContextStore();
+  const [bookContent, setBookContent] = useState("");
+  console.log(bookIndex);
+  console.log("-----books------");
+  console.log(books);
+  const book = books.find((book) => book.title === bookContext.bookTitle);
 
-                     setGlobalBookTopic(item.topic)
-                     setGlobalBookSubTopic(subtopic)
-                     setGlobalContent(contentResponse.data)
-                   }}>
-                     Get Content
-                   </button>
-                   </li>
-                 ))}
-               </ul>
-             </div>
-           ))
-         }
-    </div>
+  return <div>
+    {
+      book && book.bookTopic.map((item: BookTopic) => (
+        <div key={item.topic}>
+          <h1>topic - {item.topic}</h1>
+          <ul>
+            {item.subtopics.map((subtopic: BookSubTopic) => (
+              <li key={subtopic.subtopic}>
+                {subtopic.subtopic}
+                <button onClick={async () => {
+                  const contentResponse = await axios.get("/api/book-content", {
+                    params: {
+                      //    bookTitle: bookTitle,
+                      bookTopic: item.topic,
+                      bookSubTopic: subtopic.subtopic,
+                    },
+                  });
+
+                  console.log("contentResponse", contentResponse.data);
+                  setBookContent(contentResponse.data);
+
+                  setGlobalBookTopic(item.topic)
+                  setGlobalBookSubTopic(subtopic.subtopic)
+                  setGlobalContent(contentResponse.data)
+                }}>
+                  Get Content
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))
+    }
+  </div>
 }
