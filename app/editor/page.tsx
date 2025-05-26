f16887749405e1df004f5f16177ece6c2f3ed51b
Mayank Negi <mayankn792@gmail.com>
2025-05-27 01:12:54 +0530

    removed side indexes in main page

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -5,11 +5,15 @@ import BookIndex from "@/components/BookIndex";
 
 export default function BookEditor() {
     return <div className="flex flex-row">
-        <div className="basis-3/12"> 
+        {/* <div className="basis-3/12"> 
             <BookIndex></BookIndex>
         </div>
         <div className="basis-9/12">
             <BookContent></BookContent>      
+        </div> */}
+
+        <div>
+            <BookContent></BookContent> 
         </div>
     </div>
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -19,6 +19,7 @@ const NavBar: React.FC = () => {
                     <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content">
                         {/* Page content here */}
+                        
                         <label htmlFor="my-drawer">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
